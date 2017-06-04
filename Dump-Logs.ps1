53bc7a92f3646f8113bb8e54c9c4ffa8fa735170
AlumiNoobie <peter.di.giorgio00@gmail.com>
2017-06-04 06:38:27 -0400

    New script.
    This is the first draft of the script.

---
+++ Dump-Logs.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+Dump-Logs retrives all log files with a Record Count greater than 0, 
+dumps the logs to CSV files, and archives all the CSV files into a
+ZIP file. 
+.DESCRIPTION
+PowerShell script or module to dump every windows log on a system with 
+entries.  The script will query a remote or local system for all logs 
+with  entries.  The identified logs will be written to a CSV, archived 
+in a .zip file, and returned to the system running the script.
+.PARAMETER path
+The path to save the ZIP archive.  Default is user Documents.
+.EXAMPLE
+Dump-Logs -path C:\Users\example\Desktop
+#>
+[CmdletBinding()]
+ 
+param (
+	[string]$path
+)
+
+Get-WinEvent -listlog * | where {$_.recordcount -gt '0'}
