feb8d3916572140571ed78e660dfeb968b1a3ac3
AlumiNoobie <peter.di.giorgio00@gmail.com>
2017-06-25 10:16:37 -0400

    Tests Path
    Tests the path variable and creates path

--- Dump-Logs.ps1
+++ Dump-Logs.ps1
@@ -3,21 +3,35 @@
 Dump-Logs retrives all log files with a Record Count greater than 0, 
 dumps the logs to CSV files, and archives all the CSV files into a
 ZIP file. 
+
 .DESCRIPTION
 PowerShell script or module to dump every windows log on a system with 
 entries.  The script will query a remote or local system for all logs 
 with  entries.  The identified logs will be written to a CSV, archived 
 in a .zip file, and returned to the system running the script.
+
 .PARAMETER path
 The path to save the ZIP archive.  Default is user Documents.
 .EXAMPLE
 Dump-Logs -path C:\Users\example\Desktop
 #>
+
 [CmdletBinding()]
- 
+
+#Establish parameters for path
 param (
 	[string]$path
 )
+
+#$VerbosePreference = "Continue"
+
+#Test path and create it if required
+If(!(test-path $path))
+{
+Write-Information -MessageData "Path does not exist.  Creating Path..." -InformationAction Continue;
+New-Item -ItemType Directory -Force -Path $path | Out-Null;
+Write-Information -MessageData "...Complete" -InformationAction Continue
+}
 
 $LogList = Get-WinEvent -listlog * | where {$_.recordcount -gt '0'} | ForEach-Object {$_.LogName}
 
